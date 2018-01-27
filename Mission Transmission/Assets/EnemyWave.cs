5fbe1a02d1e896667ed456558fb9a3635da3ad0e
Amrit <amritpal.aujla@hotmail.ca>
2018-01-27 16:23:03 -0500

    Started with collision

--- Mission Transmission/Assets/EnemyWave.cs
+++ Mission Transmission/Assets/EnemyWave.cs
@@ -2,17 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerWaveStart : MonoBehaviour {
+public class EnemyWave : MonoBehaviour {
     public float speed;
+    public int power;
+    Rigidbody2D rb;
 
 	// Use this for initialization
 	void Start () {
+        power = 3;
         speed = 10;
-        var rb = GetComponent<Rigidbody2D>();
+        rb = GetComponent<Rigidbody2D>();
         rb.velocity = transform.right * speed;
-	}
+    }
 	
 	// Update is called once per frame
 	void Update () {
 	}
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        print("HA");
+    }
 }
---
+++ Mission Transmission/Assets/WaveManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveManager : MonoBehaviour {
+    public float speed;
+    public int power;
+    Rigidbody2D rb;
+
+    // Use this for initialization
+    void Start () {
+        power = 5;
+        speed = 10;
+        rb = GetComponent<Rigidbody2D>();
+        rb.velocity = transform.right * speed;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.name == "EnemyShot")
+        {
+            EnemyWave enemy = collision.gameObject.GetComponent<EnemyWave>();
+            if (enemy.power > this.power)
+            {
+                Destroy(gameObject);
+            }
+            else if (enemy.power < this.power)
+            {
+                Destroy(collision.gameObject);
+            }
+            else
+            {
+                print("Enemy hit");
+            }
+        }
+        else if (collision.gameObject.name == "PlayerShot")
+        {
+
+        }
+        else if (collision.gameObject.name == "right tower")
+        {
+            print("tower hit");
+        }
+    }
+}
