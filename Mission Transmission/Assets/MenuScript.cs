ff1fd90bef92a1827e220c9cb9b422e408a480d3
Suhas Chalotra <suhas.chalotra@mail.utoronto.ca>
2018-01-28 13:54:52 -0500

    Game is working :)

--- Mission Transmission/Assets/MenuScript.cs
+++ Mission Transmission/Assets/MenuScript.cs
@@ -8,4 +8,9 @@ public class MenuScript : MonoBehaviour {
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
 }
