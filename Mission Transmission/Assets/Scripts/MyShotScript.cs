1fd7155a9ac7bfd4156e8d203499d320d5b22ce6
Suhas Chalotra <suhas.chalotra@mail.utoronto.ca>
2018-01-27 19:39:30 -0500

    yy

---
+++ Mission Transmission/Assets/Scripts/MyShotScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyShotScript : MonoBehaviour {
+    
+	// Use this for initialization
+	void Start () {
+
+        // Get the rigidbody component
+        Rigidbody2D r2d = GetComponent<Rigidbody2D>();
+
+        // Make the bullet move upward
+        r2d.velocity = new Vector3(10 , 0, 0);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Mission Transmission/Assets/Scripts/ShooterScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShooterScript : MonoBehaviour {
+    public GameObject MyShot;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+       
+        
+        // Get the rigidbody component
+        Rigidbody2D r2d = GetComponent<Rigidbody2D>();
+
+        // Move the spaceship when an arrow key is pressed
+        if (Input.GetKey(KeyCode.W))
+            r2d.velocity = new Vector3(0, 10, 0);
+        else if (Input.GetKey(KeyCode.S))
+            r2d.velocity = new Vector3(0, -10, 0);
+        else
+            r2d.velocity = new Vector3(0, 0, 0);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(MyShot, transform.position, Quaternion.identity);
+        }
+ 
+
+
+    }
+}
