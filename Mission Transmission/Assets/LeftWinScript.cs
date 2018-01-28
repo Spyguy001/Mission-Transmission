e13c99dbaf2f4bfcfdd29e4310f8088d5e5ef06a
Suhas Chalotra <suhas.chalotra@mail.utoronto.ca>
2018-01-28 12:33:09 -0500

    End Screens for both sides

---
+++ Mission Transmission/Assets/LeftWinScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LeftWinScript : MonoBehaviour {
+
+    
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(LoadSceneAfterDelay(3));
+
+    }
+
+    IEnumerator LoadSceneAfterDelay(float waitbySecs)
+    {
+
+        yield return new WaitForSeconds(waitbySecs);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 2);
+    }	
+	
+}
